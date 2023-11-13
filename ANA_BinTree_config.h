14df34b06a43b05bb0422a32c2438fc591b845dc
SeveratheDuck <antipov.aa@phystech.edu>
2023-11-13 13:57:57 +0300

    config_file

---
+++ ANA_BinTree_config.h
@@ -0,0 +1,9 @@
+#ifndef ANA_BINTREE_CONFIG_H
+#define ANA_BINTREE_CONFIG_H
+
+typedef int ANA_BinTree_data_type;
+#define ANA_BinTree_OUTPUT_F "%d"
+
+const ANA_BinTree_data_type ANA_BinTree_POISON = -31415926;
+
+#endif
