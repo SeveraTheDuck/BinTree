1d301fe2959afacfe47c5c96d4899266941dfe3f
SeveratheDuck <antipov.aa@phystech.edu>
2023-11-19 20:31:30 +0300

    Added allocated data array in ANA_BinTree struct. Fill it if your data was allocated.

--- include/ANA_BinTree_struct.h
+++ include/ANA_BinTree_struct.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "../ANA_BinTree_config.h"
+#include "../../ANA_BinTree_config.h"
 
 typedef size_t ANA_BinTree_error_type;
 
@@ -31,6 +31,11 @@ struct ANA_BinTree
     ANA_BinTree_node* root;
     size_t n_elem;
 
+    // Use this array if data is being allocated.
+    // Allocate it and fill this array by yourself.
+    ANA_BinTree_data_type* allocated_data [MAX_NUM_OF_ALLOCATED_DATA];
+    size_t allocated_data_number;
+
     ANA_BinTree_error_type errors;
 };
 
@@ -44,8 +49,13 @@ ANA_BinTree_CtorNode   (const ANA_BinTree_data_type   data,
                               ANA_BinTree_node* const right,
                               ANA_BinTree_node* const parent);
 
+// Do not use it to destroy the whole tree if data was allocated!
+// Use ANA_BinTree_Dtor instead.
 ANA_BinTree_error_type
 ANA_BinTree_DestroySubtree (ANA_BinTree_node* const node);
+
+void
+ANA_BinTree_Dtor (ANA_BinTree* const tree);
 
 ANA_BinTree_node*
 ANA_BinTree_InsertSorted (      ANA_BinTree*          const tree,
